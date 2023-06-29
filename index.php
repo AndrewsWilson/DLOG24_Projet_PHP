9dc60d3d73ff99f280f909e3bb05028e09fdf2dc
AndrewsWilson <andrewwilson@hotmail.Fr>
2023-06-29 15:07:34 +0200

    Etape 3 : architecture logiciel + frontcontroller

--- index.php
+++ index.php
@@ -3,22 +3,27 @@ echo "Bienvenue sur le blog";
 
 
 # == Frontcontroller == #
+$action = filter_input(INPUT_GET, 'action', FILTER_SANITIZE_STRING);
+## Ici je stock toutes la/les requettes GET['action'] dans la variable $action
 
-if($_GET['action'] == ''){
-require ;
-}
-if($_GET['action'] == ''){
-require ;
-}
-else if ($_GET['action'] == ''){
-require ;
-}
-else if ($_GET['action'] == ''){
-require ;
-}
-else if ($_GET['action'] == ''){
-require ;
-}
-else{
-echo "error 404";
-}
+$routes = [
+    # Crée un tableau un tableau associatif dans lequel je stock toutes les root possible avec une clé définit
+    'home' => 'home.php';
+    'contact' => 'contact.php';
+    'about' => 'about.php';
+    // ajouter des 'clés' => 'routes' routes ici
+]
+
+
+ if(!array_key_exists($action,$routes)){
+    #Si la valeur $action ne correspond a aucunes des clés dans $route ALORS 
+    #l'action n'existe pas
+    #affichage page 404
+
+ }else{
+    # SINON SI LA VALEUR $action = est égale a des clés de route[]
+    # la variable page prendra la valeur de la clés qui correspond a la variable $action dans le tableau $routes
+    $page = $routes[$action];
+    include $page;
+ }
+
