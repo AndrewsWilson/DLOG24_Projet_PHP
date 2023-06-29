747fb3e44fbce9217498d0a0064e07bc66feb46e
AndrewsWilson <andrewwilson@hotmail.Fr>
2023-06-29 11:27:22 +0200

    Structure de dossier front controller

--- index.php
+++ index.php
@@ -1,2 +1,24 @@
 <?php
 echo "Bienvenue sur le blog";
+
+
+# == Frontcontroller == #
+
+if($_GET['action'] == ''){
+require ;
+}
+if($_GET['action'] == ''){
+require ;
+}
+else if ($_GET['action'] == ''){
+require ;
+}
+else if ($_GET['action'] == ''){
+require ;
+}
+else if ($_GET['action'] == ''){
+require ;
+}
+else{
+echo "error 404";
+}
