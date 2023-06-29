8b8ec8039322137d11d00fcc9d3304e7d312e092
AndrewsWilson <andrewwilson@hotmail.Fr>
2023-06-29 18:08:50 +0200

    creation architecture logiciel / creation PDO connection DB

---
+++ config/database.php
@@ -0,0 +1,7 @@
+<?php
+
+$DB_CONFIG = [
+    'DSN' => 'mysql:host=localhost;dbname=blog',
+    'USER' => 'andrews',
+    'PASS' => 'P22041985p'
+];
--- index.php
+++ index.php
@@ -1,6 +1,23 @@
 <?php
 echo "Bienvenue sur le blog";
 
+#J'appelle mon fichier contenant mes identifiant d'accès a la BDD
+require 'config/database.php';
+
+try{
+    #j'instancie ma PDO en la stockant dans ma variable $PDO que je pourrais utiliser plus tard
+    $pdo = new PDO($DB_CONFIG['DSN'],$DB_CONFIG['USER'],$DB_CONFIG['PASS']);
+    echo 'CONNEXION REUSSIT';
+}
+catch(PDOException $pe)
+{
+    #si erreur il y a cela affichera erreur : $pe(qui contiendra le message d'erreur)
+    echo 'ERREUR : ' . $pe->getMessage();
+}
+
+
+
+
 
 # == Frontcontroller == #
 $action = filter_input(INPUT_GET, 'action', FILTER_SANITIZE_STRING);
@@ -8,22 +25,28 @@ $action = filter_input(INPUT_GET, 'action', FILTER_SANITIZE_STRING);
 
 $routes = [
     # Crée un tableau un tableau associatif dans lequel je stock toutes les root possible avec une clé définit
-    'home' => 'home.php';
-    'contact' => 'contact.php';
-    'about' => 'about.php';
+    //'' => 'home.php',
+    'home' => 'home.php',
+    'contact' => 'contact.php',
+    'about' => 'about.php',
     // ajouter des 'clés' => 'routes' routes ici
-]
+];
 
-
- if(!array_key_exists($action,$routes)){
+if (!array_key_exists($action,$routes)) {
+    //echo 'ERROR 404';
     #Si la valeur $action ne correspond a aucunes des clés dans $route ALORS 
     #l'action n'existe pas
     #affichage page 404
-
- }else{
+} else {
     # SINON SI LA VALEUR $action = est égale a des clés de route[]
     # la variable page prendra la valeur de la clés qui correspond a la variable $action dans le tableau $routes
     $page = $routes[$action];
     include $page;
- }
+}
+
+
+
+
+
+
 
